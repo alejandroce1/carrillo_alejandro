a9ff5201d7c0c084d08ed3246a6fc5aa76ea132d
alejandroce1 <alejandrocarrilloe1@gmail.com>
2017-06-13 08:56:59 -0700

    update

--- lab_15/brains.js
+++ lab_15/brains.js
@@ -1,16 +1,3 @@
-funtion shapes ()
+function shapes()
 {
-	var x =document.getElementById("canvas");
-	canvas = x.getContext ("2d");
-	canvas.strokeSyle="red";
-	canvas.fillStyle= "blue";
-	canvas.beginPath();
-	canvas.moveTo(50, 50);
-	canvas.lineTo(70, 250);
-	canvas.lineTo(300, 300);
-	canvas.closePath();
-	canvas.stroke();
-	canvas.fill();
-}
-	
-window.addEventListener("load", shapes, false);
+	var x = document.getElementById
